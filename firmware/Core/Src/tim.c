87cd88b0b989a458b17742b63423c36461aa97fc
ganlu <133077713+shalebao3@users.noreply.github.com>
2026-09-18 15:33:58 +0800

    fix(tim): update TIM3 interrupt priority to resolve potential conflicts

--- firmware/Core/Src/tim.c
+++ firmware/Core/Src/tim.c
@@ -325,7 +325,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     __HAL_LINKDMA(tim_baseHandle,hdma[TIM_DMA_ID_TRIGGER],hdma_tim3_ch1_trig);
 
     /* TIM3 interrupt Init */
-    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(TIM3_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(TIM3_IRQn);
   /* USER CODE BEGIN TIM3_MspInit 1 */
 
